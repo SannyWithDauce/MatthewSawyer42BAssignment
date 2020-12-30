5f7fac8325e209a6d66855e9bebb285e337ba716
SannyWithDauce <73096469+SannyWithDauce@users.noreply.github.com>
2020-12-30 01:12:06 +0100

    Border and BG
    Added assets, made the scrolling bg, and also added the border.

---
+++ HA/HomeAssignment/Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    float xMin, xMax, yMin, yMax;
+    void Start()
+    {
+        Border();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void Border()
+    {
+        Camera gameCamera = Camera.main;
+
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x;
+
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y;
+    }
+
+}
---
+++ HA/HomeAssignment/Assets/Scripts/Scroll.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll : MonoBehaviour
+{
+
+    public float speed = 0.5f;
+
+    private MeshRenderer mesh_Renderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh_Renderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame 
+    void Update()
+    {
+        float y = Time.time * speed;
+        
+        Vector2 offset = new Vector2(0, y);
+
+        mesh_Renderer.sharedMaterial.SetTextureOffset("_MainTex", offset);
+    }
+}
